0433b1a140a1c636bdfc98e533e5b70aa7ff905a
zizimahmoudemam203007-prog <zizimahmoudemam203007@gmail.com>
2026-05-11 14:39:49 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,25 +1,33 @@
 function changeTheme(){
     document.body.classList.toggle("dark-theme");
-
+    
     if(document.body.classList.contains("dark-theme")){
-        localStorage.setItem("theme", "dark");
+        localStorage.setItem("theme","dark");
     }
     else{
-        localStorage.setItem("theme", "light");
+        localStorage.setItem("theme","light");
     }
 } 
-
 function changeLayout(){
-    document.body.classList.toggle("vertical-layout");
-
+   document.body.classList.toggle("vertical-layout");
     if(document.body.classList.contains("vertical-layout")){
-        localStorage.setItem("layout", "vertical");
+        localStorage.setItem("layout","vertical");
     }
     else{
-        localStorage.setItem("layout", "horizontal");
+        localStorage.setItem("layout","horizontal");
     }
-}
+ }
+window.onload =function(){
+    let theme = localStorage.getItem("theme");
+    let layout= localStorage.getItem("layout");
 
+    if(theme==="dark"){
+        document.body.classList.add("dark-theme");
+    }
+   if(layout==="vertical"){
+        document.body.classList.add("vertical-layout");
+   }
+};
 const menuIcon = document.getElementById("menuIcon");
 const navMenu = document.getElementById("navMenu");
 
@@ -27,10 +35,8 @@ menuIcon.addEventListener("click", function() {
     navMenu.classList.toggle("active")
 });
 
-function goToPage(page) {  
-    window.location.href = page;
+function goToPage(page) {  window.location.href = page;
 }
-
 const form = document.getElementById("bookingForm");
 
 form.addEventListener("submit", function(e){
@@ -41,17 +47,4 @@ form.addEventListener("submit", function(e){
         e.preventDefault();
         alert("Please fill all required fields");
     }
-});
-
-
-window.addEventListener("load", function(){
-
-    if(localStorage.getItem("theme") === "dark"){
-        document.body.classList.add("dark-theme");
-    }
-
-    if(localStorage.getItem("layout") === "vertical"){
-        document.body.classList.add("vertical-layout");
-    }
-
 });
